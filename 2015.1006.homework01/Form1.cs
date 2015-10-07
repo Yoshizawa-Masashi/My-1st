4d681cb6910d5bc49c96dd2021b9ad5a4b6ba3d1
yoshizawa-Masashi <s1323092@kanagawa-it.ac.jp>
2015-10-07 16:56:01 +0900

    10月6日分課題　未完成状態

---
+++ 2015.1006.homework01/Form1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _2015._1006.homework01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
